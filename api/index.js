1cf73be6b729818defe2edb53ccadbcf2cccfe05
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-10-16 17:42:31 -0500

    Implemented various endpoints: login, profile, register, etc, connected to the DB and made front end updates to be in sync with backend

--- api/index.js
+++ api/index.js
@@ -1,13 +1,67 @@
 const express = require("express");
 const cors = require("cors");
-
+const { default: mongoose } = require("mongoose");
+const User = require("./models/User");
 const app = express();
-app.use(cors());
-app.use(express.json());
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
-app.post("/register", (req, res) => {
+const salt = bcrypt.genSaltSync(10);
+const secret = "sjekof843utjre";
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect(
+  `mongodb+srv://vpalatnyk:uVo91pGqwkv438FY@cluster0.lcaly6w.mongodb.net/?retryWrites=true&w=majority`
+);
+
+// Code below: registers a user and write to the database encrypting users password)
+
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  res.json({ requestData: { username, password } });
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+});
+
+// 1. we grab username and password from a request and compare it to the values in the database(line 36)
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("Ok");
+    });
+  } else {
+    res.status(400).json("Wrong credentials.");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+  res.json(req.cookies);
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("Ok");
 });
 
 app.listen(4000);
