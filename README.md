253428e26ba19c6ed3a1a1ba82c0a642f3e72a79
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-10-16 13:27:20 -0500

    Built basic front end with React and started implementing Register endpoint

---
+++ README.md
@@ -0,0 +1,6 @@
+### First full-stack project
+
+# Using React, Node, Express and Mongo
+
+npx nodemon project-name - start back-end OR node project-name
+yarn start - to start front-end
---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(e) {
+    e.preventDefault();
+    await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  return (
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        value={username}
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button>Register</button>
+    </form>
+  );
+}
