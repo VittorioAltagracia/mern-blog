c337ca0b0413a2b17992e8dd169285f8435ba56d
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-10-20 18:07:41 -0500

    work to account for editing posts, creating new posts, etc

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -1,23 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import Editor from "../Editor";
+import { useParams } from "react-router-dom";
 
-export default function CreatePost() {
+export default function EditPost() {
+  const { id } = useParams();
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
   const [files, setFiles] = useState("");
+  const [cover, setCover] = useState("");
   const [redirect, setRedirect] = useState(false);
 
-  async function createNewPost(e) {
+  useEffect(() => {
+    fetch("http://localhost:4000/post/" + id).then((response) => {
+      response.json().then((postInfo) => {
+        setTitle(postInfo.title);
+        setContent(postInfo.content);
+        setSummary(postInfo.summary);
+      });
+    });
+  }, []);
+
+  async function updatePost(e) {
+    e.preventDefault();
     const data = new FormData();
     data.set("title", title);
     data.set("summary", summary);
     data.set("content", content);
-    data.set("file", files[0]);
-    e.preventDefault();
+    data.set("id", id);
+    if (files?.[0]) {
+      data.set("file", files?.[0]);
+    }
     const response = await fetch("http://localhost:4000/post", {
-      method: "POST",
+      method: "PUT",
       body: data,
       credentials: "include",
     });
@@ -27,11 +43,11 @@ export default function CreatePost() {
   }
 
   if (redirect) {
-    return <Navigate to={"/"} />;
+    return <Navigate to={`/post/${id}`} />;
   }
 
   return (
-    <form onSubmit={createNewPost}>
+    <form onSubmit={updatePost}>
       <input
         type="title"
         placeholder="Title"
@@ -46,8 +62,7 @@ export default function CreatePost() {
       />
       <input type="file" onChange={(e) => setFiles(e.target.files)} />
       <Editor onChange={setContent} value={content} />
-
-      <button style={{ marginTop: "5px" }}>Create post</button>
+      <button style={{ marginTop: "5px" }}>Update post</button>
     </form>
   );
 }
